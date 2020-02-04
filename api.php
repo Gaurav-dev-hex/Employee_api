f90ed9e5ec8349854113726c689e8f7509eff6b0
Gaurav-dev-hex <53593529+Gaurav-dev-hex@users.noreply.github.com>
2020-02-04 08:40:46 +0530

    Employee data
    create a folder called css and add css file in that folder

---
+++ api.php
@@ -0,0 +1,43 @@
+<?php
+header("Content-Type:application/json");
+
+if (isset($_GET['id']) && $_GET['id']!="") {
+
+	
+	include('db.php');
+	$empId = $_GET['id'];
+	
+	$sql = "SELECT * FROM employee.emp WHERE id=$empId";
+	
+	$result = $conn->query($sql);
+	
+
+	if(mysqli_num_rows($result)>0){
+		
+	while($row = mysqli_fetch_array($result))
+	{
+	$id= $row['id'];
+	$name = $row['name'];
+	$age = $row['age'];
+	$salary = $row['salary'];
+	response($id,$name,$age,$salary);
+	}
+	mysqli_close($con);
+	
+	}else {
+		response(NULL, NULL, 200,"No Record Found");
+		}
+}else{
+	response(NULL, NULL, 400,"Invalid Request");
+	}
+
+function response($id,$name,$age,$salary){
+	$response['id'] = $id;
+	$response['name'] = $name;
+	$response['age'] = $age;
+	$response['salary'] = $salary;
+	
+	$json_response = json_encode($response);
+	echo $json_response;
+}
+?>
